b00d7ab07579e80d42ded5b4427317f235e8cce7
IliesT <47112446+ilies-t@users.noreply.github.com>
2019-07-12 19:57:24 +0200

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Simple Spotify scraper  
+Return the name of a playlist and the number of times an artist is present in a playlist to "result.json".  
+  
+## Prerequisites  
+#### Selenium  
+```shell
+pip install selenium
+```  
+#### Chrome webdriver  
+* [Chromedriver](https://chromedriver.storage.googleapis.com/index.html?path=75.0.3770.90/) - Works with v.75.0.3770.90  
+  
+## Usage  
+Return one playlist in JSON, **use only the playlist ID** :
+```python
+scrape_playlist("37i9dQZF1DX7Ku6cgJPhh5?si=HCwok0iiTvykUVQlCXjjcg")
+```  
+If they are multiple playlist, put "last=True" at the last :  
+```python
+# last=True
+scrape_playlist("37i9dQZF1DX7Ku6cgJPhh5?si=HCwok0iiTvykUVQlCXjjcg", last=True)
+```  
+## License
+[Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)
---
+++ main.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from time import sleep
+from collections import Counter
+from json import dumps
+
+# open selenium & file
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--incognito")
+driver = webdriver.Chrome(options=chrome_options)
+fp = open("result.json", "w")
+fp.write("{")
+sleep(1)
+
+def scrape_playlist(playlist_id, last=False):
+    # open driver
+    driver.get("https://open.spotify.com/playlist/"+playlist_id)
+
+    # create list
+    artist_list = []
+
+    # parse HTML content with Xpath
+    artist_value = driver.find_elements_by_xpath('//a[@class="tracklist-row__artist-name-link"]')
+    for artist in artist_value:
+        artist = artist.get_attribute('innerHTML')
+        artist_list.append(artist)
+    playlist_name = driver.find_element_by_xpath('//div[@class="TrackListHeader__entity-name"]/h2/span').get_attribute('innerHTML')
+
+    # count result and write on file
+    count_artist = dumps(Counter(artist_list), sort_keys=True, indent=12)
+    
+    # False parameters for JSON
+    if last == True:
+        fp.write('\n\t"{}":\n\t\t{}\n}}\n'.format(playlist_name ,count_artist))
+    elif last == False:
+        fp.write('\n\t"{}":\n\t\t{},'.format(playlist_name ,count_artist))
+
+# scrape
+scrape_playlist("37i9dQZF1DX7Ku6cgJPhh5?si=HCwok0iiTvykUVQlCXjjcg")
+scrape_playlist("37i9dQZF1DWVuV87wUBNwc?si=3cwXbM5MQSauQAVxVeS11g", last=True)
+
+# finish
+fp.close()
+driver.quit()
+print("FINISH")
