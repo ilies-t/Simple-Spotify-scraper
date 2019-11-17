847832046035bde7894d0cc9dec0cecdefca4cb7
IliesT <47112446+ilies-t@users.noreply.github.com>
2019-11-17 22:28:06 +0100

    It wasn't much, but it was honest work.

--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 from selenium import webdriver
-from time import sleep
 from collections import Counter
-from json import dumps
+from json import dump
 
 # open selenium & file
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
 driver = webdriver.Chrome(options=chrome_options)
 fp = open("result.json", "w")
-fp.write("{")
-sleep(1)
+playlist_wanted = [
+    "37i9dQZF1DX2A29LI7xHn1?si=qT3X5wUCRDCXEhpJyg2_Mw",
+    "37i9dQZF1DX7Ku6cgJPhh5?si=HCwok0iiTvykUVQlCXjjcg",
+    "37i9dQZF1DXcBWIGoYBM5M?si=GqVK28qqQ2WQ4tB2060Ldg"
+    ]
 
-def scrape_playlist(playlist_id, last=False):
-    # open driver and create list
-    driver.get("https://open.spotify.com/playlist/"+playlist_id)
-    artist_list = []
+def scrape_playlist(playlist_array):
+    # count_artist is the list with all of playlist
+    count_artist = []
 
-    # parse HTML content with Xpath
-    artist_value = driver.find_elements_by_xpath('//a[@class="tracklist-row__artist-name-link"]')
-    for artist in artist_value:
-        artist = artist.get_attribute('innerHTML')
-        artist_list.append(artist)
-    playlist_name = driver.find_element_by_xpath('//div[@class="TrackListHeader__entity-name"]/h2/span').get_attribute('innerHTML')
+    # start loop for all playlist in playlist_wanted
+    for p in playlist_array:
+        # open driver and create artist list
+        driver.get("https://open.spotify.com/playlist/"+p)
+        artist_list = []
 
-    # count result and write on file
-    count_artist = dumps(Counter(artist_list), sort_keys=True, indent=12)
+        # parse HTML content with Xpath
+        artist_value = driver.find_elements_by_xpath('//a[@class="tracklist-row__artist-name-link"]')
+        for artist in artist_value:
+            artist = artist.get_attribute('innerHTML')
+            artist_list.append(artist)
+        playlist_name = driver.find_element_by_xpath('//div[@class="TrackListHeader__entity-name"]/h2/span').get_attribute('innerHTML')
 
-    # False parameters for JSON
-    if last == True:
-        fp.write('\n\t"{}":\n\t\t{}\n}}\n'.format(playlist_name ,count_artist))
-    elif last == False:
-        fp.write('\n\t"{}":\n\t\t{},'.format(playlist_name ,count_artist))
+        # count result and write on file
+        p_result = {playlist_name: Counter(artist_list)}
+        count_artist.append(p_result)
 
-# scrape
-scrape_playlist("37i9dQZF1DX7Ku6cgJPhh5?si=HCwok0iiTvykUVQlCXjjcg")
-scrape_playlist("37i9dQZF1DWVuV87wUBNwc?si=3cwXbM5MQSauQAVxVeS11g", last=True)
+    c = dump(count_artist, sort_keys=True, indent=4, fp=fp)
+    fp.close()
+    driver.quit()
+
+# scrape and see result !
+scrape_playlist(playlist_wanted)
 
 # finish
-fp.close()
-driver.quit()
-print("FINISH")
+print("finish :)")
